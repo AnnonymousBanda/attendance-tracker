925d08049d64940b327b39e45cbdadf3a3119fb2
VinayKhedkar <arithmetic.chip@gmail.com>
2025-03-11 23:06:35 +0530

    init: set up default styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,11 +2,16 @@ import { fontFamily } from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,jsx}'],
+    content: ['./index.html', './src/**/*.{js,jsx,css}'],
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Plus Jakarta Sans', ...fontFamily.sans],
+            },
+            colors: {
+                primary: '#f3f8fa',
+                secondary: '#29303d',
+                tertiary: '#a3bbd6',
             },
         },
     },
