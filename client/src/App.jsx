8542e813c58e2f05c279e51c279a7b47e357f5c7
VinayKhedkar <arithmetic.chip@gmail.com>
2025-03-12 00:21:36 +0530

    fix: fix layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
-import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import { Route, BrowserRouter, Routes, useLocation } from 'react-router-dom'
 import { Home, NotFound, Schedule, Stats, Timetable } from './pages'
 import Navbar from './components/Navbar'
+
+const Layout = ({ children }) => {
+    const pathname = useLocation()
+
+    return (
+        <div className="w-screen h-screen flex flex-col gap-[1rem]">
+            <div className="w-full h-full overflow-auto">{children}</div>
+            {pathname.pathname !== '/' && <Navbar />}
+        </div>
+    )
+}
 
 const App = () => {
     return (
         <BrowserRouter>
-            <div className="w-screen h-screen flex flex-col gap-[1rem]">
-                <div className="w-full h-full overflow-auto">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/schedule" element={<Schedule />} />
-                        <Route path="/stats" element={<Stats />} />
-                        <Route path="/timetable" element={<Timetable />} />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </div>
-                <Navbar />
-            </div>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/schedule" element={<Schedule />} />
+                    <Route path="/stats" element={<Stats />} />
+                    <Route path="/timetable" element={<Timetable />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Layout>
         </BrowserRouter>
     )
 }
