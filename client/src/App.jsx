592d565994429b5c6ce86ae6599910082a98c7e0
Ankit Bhagat <annonymous007banda@gmail.com>
2025-03-12 23:44:46 +0530

    fix[App]: fix the layout bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,35 +3,31 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import { Home, NotFound, Schedule, Stats, Timetable, Login } from './pages'
 import { Navbar } from './components'
 import Header from './components/Header'
+import { Outlet } from 'react-router-dom'
 
-const Layout = ({ children }) => {
-    return (
-        <div className="w-screen h-svh flex flex-col gap-[1rem]">
-            <Header />
-            <div className="w-full h-full">{children}</div>
-            <Navbar />
+const Layout = () => (
+    <div className="w-screen h-svh flex flex-col gap-[1rem]">
+        <Header />
+        <div className="w-full h-full">
+            <Outlet />
         </div>
-    )
-}
+        <Navbar />
+    </div>
+)
 
-const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<Layout />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/schedule" element={<Schedule />} />
-                    <Route path="/stats" element={<Stats />} />
-                    <Route path="/timetable" element={<Timetable />} />
-                </Route>
-
-                <Route>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="*" element={<NotFound />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
-}
+const App = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/schedule" element={<Schedule />} />
+                <Route path="/stats" element={<Stats />} />
+                <Route path="/timetable" element={<Timetable />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    </BrowserRouter>
+)
 
 export default App
