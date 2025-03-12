1ec97b09f0efc2376de2c4c6767502cc47049498
Anjali <anjaliushadubey64@gmail.com>
2025-03-12 17:35:17 +0530

    fix(navbar): fix active state color change issue

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,26 +1,45 @@
-import React from 'react'
-
-import { Home, Library, Present, Schedule } from '../assets'
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Home, Library, Present, Schedule } from "../assets";
 
 const Navbar = () => {
-    return (
-        <nav className="fixed bottom-0 left-0 w-full bg-white shadow-md border-t py-2">
-            <div className="flex justify-around items-center">
-                <button className="text-gray-600 hover:text-black">
-                    <img src={Home} alt="home" />
-                </button>
-                <button className="text-gray-600 hover:text-black">
-                    <img src={Library} alt="library" />
-                </button>
-                <button className="text-gray-600 hover:text-black">
-                    <img src={Present} alt="present" />
-                </button>
-                <button className="text-gray-600 hover:text-black">
-                    <img src={Schedule} alt="schedule" />
-                </button>
-            </div>
-        </nav>
-    )
-}
+  const location = useLocation();
+  const isActive = (path) => location.pathname === path;
 
-export default Navbar
+  return (
+    <nav className="fixed bottom-0 left-0 w-full bg-white shadow-md border-t py-[2rem]">
+      <div className="flex justify-around items-center">
+        <Link to="/" className="text-gray-600 hover:text-black">
+          <img
+            src={Home}
+            alt="home"
+            className={`h-[6rem] w-[6rem] ${isActive("/") ? "" : "opacity-50"}`}
+          />
+        </Link>
+        <Link to="/stats" className="text-gray-600 hover:text-black">
+          <img
+            src={Library}
+            alt="stats"
+            className={`h-[6rem] w-[6rem] ${isActive("/stats") ? "" : "opacity-50"}`}
+          />
+        </Link>
+        <Link to="/timetable" className="text-gray-600 hover:text-black">
+          <img
+            src={Present}
+            alt="timetable"
+            className={`h-[6rem] w-[6rem] ${isActive("/timetable") ? "" : "opacity-50"}`}
+          />
+        </Link>
+        <Link to="/schedule" className="text-gray-600 hover:text-black">
+          <img
+            src={Schedule}
+            alt="schedule"
+            className={`h-[6rem] w-[6rem] ${isActive("/schedule") ? "" : "opacity-50"}`}
+          />
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
