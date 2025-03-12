c51595e2b8e7661ac6136a790fcf22685cc0c9c3
Ankit Bhagat <annonymous007banda@gmail.com>
2025-03-12 14:13:36 +0530

    fix[assests]: fixed index.js imports, deletes depreciated png

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,6 +1,10 @@
-import Home from "./home.svg";
-import Library from "./library.svg";
-import Schedule from "./schedule.svg";
-import Present from "./present.svg";
+import Home from './home.svg'
+import Library from './library.svg'
+import Schedule from './schedule.svg'
+import Present from './present.svg'
+import msLogo from './ms_logo.svg'
+import logo from './logo.svg'
+import addIcon from './addIcon.png'
+import closeIcon from './closeIcon.png'
 
-export  {Home,Library,Schedule,Present};
+export { Home, Library, Schedule, Present, msLogo, logo, addIcon, closeIcon }
