1d5bd34e9d648f30cfc785a596a9c8cf4952e286
Ankit Bhagat <annonymous007banda@gmail.com>
2025-03-12 14:24:40 +0530

    feat[pages]: add Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Route, BrowserRouter, Routes } from 'react-router-dom'
-import { Home, NotFound, Schedule, Stats, Timetable } from './pages'
+import { Route, BrowserRouter, Routes, useLocation } from 'react-router-dom'
+import { Home, NotFound, Schedule, Stats, Timetable, Login } from './pages'
 import Navbar from './components/Navbar'
 
 const Layout = ({ children }) => {
+    const pathname = useLocation()
+
     return (
-        <div className="w-svw h-svh flex flex-col gap-[1rem]">
+        <div className="w-screen h-svh flex flex-col gap-[1rem]">
             <div className="w-full h-full">{children}</div>
-            <Navbar />
+            {pathname.pathname !== '/login' && <Navbar />}
         </div>
     )
 }
@@ -21,6 +23,7 @@ const App = () => {
                     <Route path="/schedule" element={<Schedule />} />
                     <Route path="/stats" element={<Stats />} />
                     <Route path="/timetable" element={<Timetable />} />
+                    <Route path="/login" element={<Login />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </Layout>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,6 +2,7 @@ import Home from './Home'
 import Schedule from './Schedule'
 import Stats from './Stats'
 import Timetable from './Timetable'
+import Login from './Login'
 import NotFound from './NotFound'
 
-export { Home, Schedule, Stats, Timetable, NotFound }
+export { Home, Schedule, Stats, Timetable, Login, NotFound }
