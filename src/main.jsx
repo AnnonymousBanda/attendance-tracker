ad26d28481bf568ce9bf5211384d5122014f96fa
Ankit Bhagat <annonymous007banda@gmail.com>
2025-03-13 21:28:54 +0530

    chore[sw]: moved sw script to public dir

--- src/main.jsx
+++ src/main.jsx
@@ -12,9 +12,9 @@ createRoot(document.getElementById('root')).render(
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register('../sw.js')
+            .register('/sw.js')
             .then((registration) => {
-                console.log('Service Worker registered:', registration)
+                console.log('Service Worker registered:', registration.scope)
             })
             .catch((error) => {
                 console.log('Service Worker registration failed:', error)
