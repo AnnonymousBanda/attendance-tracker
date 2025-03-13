9625d3c2cdfde898b21bc61b9ad2ca738132aba4
Anjali <anjaliushadubey64@gmail.com>
2025-03-13 11:56:54 +0530

    feat: Add Calender and DaySelector files

--- client/src/pages/Schedule.jsx
+++ client/src/pages/Schedule.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import DaySelector from '../components/DaySelector'
 
 export default function Schedule() {
-    return <div>Schedule</div>
+    const daysList = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    return (
+        <div>
+            <DaySelector days={daysList} />
+        </div>
+    )
 }
--- client/src/pages/Timetable.jsx
+++ client/src/pages/Timetable.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import Calender from '../components/Calender'
 
 const Timetable = () => {
-	return <div>Timetable</div>
+    const daysData = [
+        { name: 'Mon', date: '7' },
+        { name: 'Tue', date: '7' },
+        { name: 'Wed', date: '7' },
+        { name: 'Thu', date: '7' },
+        { name: 'Fri', date: '7' },
+        { name: 'Sat', date: '7' },
+    ]
+
+    return (
+        <div>
+            <Calender days={daysData} />
+        </div>
+    )
 }
 
 export default Timetable
