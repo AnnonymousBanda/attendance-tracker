4a759cb8e7da87cf73f4c4f73ff027ab508d7e5d
Ankit Bhagat <annonymous007banda@gmail.com>
2025-03-13 20:52:25 +0530

    feat: add imports for all the components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,7 @@
 import Lecture from './Lecture'
 import Navbar from './Navbar'
+import Calender from './Calender'
+import DaySelector from './DaySelector'
+import Header from './Header'
 
-export { Lecture, Navbar }
+export { Calender, DaySelector, Header, Lecture, Navbar }
