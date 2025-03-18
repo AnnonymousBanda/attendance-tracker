c103a32f586ffce770bacc082570ac2472c243e4
khushid830 <khushid830@example.com>
2025-03-18 20:07:57 +0530

    fix:css style of lecture

--- src/components/Lecture.jsx
+++ src/components/Lecture.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
+import { MdClose } from 'react-icons/md'
 
 const Lecture = ({ fro, to, sub_code, sub_name }) => {
     return (
-        <div className="max-container border-l-5 relative bg-[#F5F5F5] rounded-xl px-6 py-8 shadow-lg shadow-[#a0b8d9] font-[Plus Jakarta Sans] hover:shadow-[#a0b8d9]/20 height-auto  transition-shadow">
+        <div className=" border-l-5 relative bg-[#F5F5F5] rounded-xl px-6 py-8 shadow-lg shadow-[#b1c1d5]  w-full  height-auto  transition-shadow">
             <div className="absolute left-[-55px] top-1/2 -translate-y-1/2 text-right space-y-1">
-                <span className="block text-[1.1rem] font-semibold text-gray-900">
-                    {fro}
-                </span>
-                <span className="block text-[0.9rem] text-gray-600">{to}</span>
+                <p className=" font-bold text-gray-900">{fro}</p>
+                <p className="block text-gray-600">{to}</p>
             </div>
             <div className="absolute right-3 top-3 cursor-pointer text-gray-500 hover:text-gray-900 transition-colors text-xl font-semibold">
-                ✕
+                <MdClose size={24} />
             </div>
-            <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-[2.5rem]">
                 <h2>
-                    {sub_code}:{sub_name}
+                    {sub_code}: {sub_name}
                 </h2>
                 <div className="flex gap-4 flex-wrap">
                     <button className="bg-green-100 text-green-700 font-medium py-2 px-6 rounded-lg hover:bg-green-200 transition-colors duration-200">
