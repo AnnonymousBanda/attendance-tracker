e6654b866548ecf8b2409a1a2d20b924a3442b77
khushid830 <khushid830@example.com>
2025-03-18 19:40:20 +0530

    fix:css of lecture component

--- src/components/Lecture.jsx
+++ src/components/Lecture.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Lecture = ({ fro, to, sub_code, sub_name }) => {
     return (
-        <div className="max-container border-l-5 relative bg-[#F5F5F5] rounded-xl px-6 py-8 shadow-lg shadow-[#a0b8d9] font-[Plus Jakarta Sans] hover:shadow-[#a0b8d9]/20 transition-shadow">
+        <div className="max-container border-l-5 relative bg-[#F5F5F5] rounded-xl px-6 py-8 shadow-lg shadow-[#a0b8d9] font-[Plus Jakarta Sans] hover:shadow-[#a0b8d9]/20 height-auto  transition-shadow">
             <div className="absolute left-[-55px] top-1/2 -translate-y-1/2 text-right space-y-1">
                 <span className="block text-[1.1rem] font-semibold text-gray-900">
                     {fro}
@@ -12,19 +12,21 @@ const Lecture = ({ fro, to, sub_code, sub_name }) => {
             <div className="absolute right-3 top-3 cursor-pointer text-gray-500 hover:text-gray-900 transition-colors text-xl font-semibold">
                 ✕
             </div>
-            <h1 className="text-lg font-semibold text-gray-900 mb-4">
-                {sub_code}:{sub_name}
-            </h1>
-            <div className="flex gap-4 flex-wrap">
-                <button className="bg-green-100 text-green-700 font-medium py-2 px-6 rounded-lg hover:bg-green-200 transition-colors duration-200">
-                    Present
-                </button>
-                <button className="bg-red-100 text-red-700 font-medium py-2 px-6 rounded-lg hover:bg-red-200 transition-colors duration-200">
-                    Absent
-                </button>
-                <button className="bg-yellow-200 text-yellow-700 font-medium py-2 px-6 rounded-lg hover:bg-yellow-100 hover:text-yellow-500 transition-colors duration-200">
-                    sick
-                </button>
+            <div className="flex flex-col gap-5">
+                <h2>
+                    {sub_code}:{sub_name}
+                </h2>
+                <div className="flex gap-4 flex-wrap">
+                    <button className="bg-green-100 text-green-700 font-medium py-2 px-6 rounded-lg hover:bg-green-200 transition-colors duration-200">
+                        <p>Present</p>
+                    </button>
+                    <button className="bg-red-100 text-red-700 font-medium py-2 px-6 rounded-lg hover:bg-red-200 transition-colors duration-200">
+                        <p>Absent</p>
+                    </button>
+                    <button className="bg-yellow-200 text-yellow-700 font-medium py-2 px-6 rounded-lg hover:bg-yellow-100 hover:text-yellow-500 transition-colors duration-200">
+                        <p>Sick</p>
+                    </button>
+                </div>
             </div>
         </div>
     )
