780eea245a6eb8a52bf626f7c9875622ed51fb79
Ankit Bhagat <annonymous007banda@gmail.com>
2025-03-20 12:56:45 +0530

    fix: fix global error handler

--- firebase/firebase.error.js
+++ firebase/firebase.error.js
@@ -2,26 +2,47 @@ class AppError extends Error {
 	constructor(message, status) {
 		super(message)
 		this.status = status
+		this.isOperational = true
 		Error.captureStackTrace(this, this.constructor)
 	}
 }
 
 const catchAsync = (fn) => {
-	return (...params) => {
+	return async (...params) => {
 		try {
-			return fn(...params)
+			return await fn(...params)
 		} catch (error) {
+			console.log('Global error handler:')
 			return globalErrorHandler(error)
 		}
 	}
 }
 
 const globalErrorHandler = (error) => {
+	if (process.env.NEXT_PUBLIC_ENV === 'development')
+		handleDevelopmentError(error)
+	else handleProductionError(error)
+}
+
+const handleDevelopmentError = (error) => {
 	return {
 		status: error.status || 500,
-		message: error.message || 'Internal server error',
-		stack: error.stack || null,
+		message: error.message,
+		stack: error.stack,
 	}
 }
 
-module.exports = { AppError, catchAsync }
+const handleProductionError = (error) => {
+	if (error.isOperational) {
+		return {
+			status: error.status,
+			message: error.message,
+		}
+	}
+	return {
+		status: 500,
+		message: 'Something went wrong',
+	}
+}
+
+export { AppError, catchAsync }
