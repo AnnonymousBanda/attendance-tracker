8e05e669411104106f1b77ff42d3f44f05833458
VinayKhedkar <arithmetic.chip@gmail.com>
2025-03-20 23:15:06 +0530

    feat: add functionality to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,26 @@
+'use client'
+import { useState } from 'react'
 import { MdOutlineSick, MdSick } from 'react-icons/md'
 import { PiHandPalmBold, PiHandPalmDuotone } from 'react-icons/pi'
 import { RxCrossCircled } from 'react-icons/rx'
 
 function Button({ lecture }) {
+    const [status, setStatus] = useState(lecture.status)
+    const handleClick = (status) => {
+        setStatus(status)
+    }
+
     return (
         <div className="flex gap-[1.5rem]">
             <button
+                onClick={() => handleClick('present')}
                 className={`bg-green text-black font-medium p-[0.5rem] rounded-lg transition-colors duration-400 cursor-pointer ${
-                    lecture.status === 'present'
+                    status === 'present'
                         ? 'hover:cursor-default opacity-40'
                         : ''
                 }`}
             >
-                <div className="flex p-[0.3rem]  justify-center items-center">
+                <div className="flex p-[0.3rem] justify-center items-center">
                     <PiHandPalmDuotone
                         strokeWidth={5}
                         stroke="#d6f6f6"
@@ -22,10 +30,9 @@ function Button({ lecture }) {
                 </div>
             </button>
             <button
+                onClick={() => handleClick('absent')}
                 className={`bg-red text-black font-medium p-[0.5rem] rounded-lg transition-colors duration-400 cursor-pointer ${
-                    lecture.status === 'absent'
-                        ? 'hover:cursor-default opacity-40'
-                        : ''
+                    status === 'absent' ? 'hover:cursor-default opacity-40' : ''
                 }`}
             >
                 <div className="flex  p-[0.3rem] justify-center items-center">
@@ -39,10 +46,9 @@ function Button({ lecture }) {
                 </div>
             </button>
             <button
+                onClick={() => handleClick('medical')}
                 className={`bg-yellow text-black font-medium p-[0.5rem] rounded-lg transition-colors duration-400 cursor-pointer ${
-                    lecture.status === 'sick'
-                        ? 'hover:cursor-default opacity-40'
-                        : ''
+                    status === 'medical' ? 'hover:cursor-default opacity-40' : ''
                 }`}
             >
                 <div className="flex  p-[0.3rem] justify-center items-center">
