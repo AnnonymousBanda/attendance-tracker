72c438d3332ef339ad0e75b76515a96816ab20f5
VinayKhedkar <arithmetic.chip@gmail.com>
2025-03-20 23:56:58 +0530

    feat: integrate button component in lectures page

--- src/components/Lecture.jsx
+++ src/components/Lecture.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { MdClose } from 'react-icons/md'
+import { Button } from '.'
 
 const Lecture = ({ fro, to, courseCode, courseName, status }) => {
     return (
@@ -15,23 +16,7 @@ const Lecture = ({ fro, to, courseCode, courseName, status }) => {
                 <h2>
                     {courseCode}: {courseName}
                 </h2>
-                <div className="flex gap-[1rem]">
-                    <button
-                        className={`bg-green-400 text-black font-medium p-[0.5rem] rounded-lg transition-colors duration-400 cursor-pointer ${status === 'present' ? 'hover:cursor-default opacity-40' : ''}`}
-                    >
-                        <p>present</p>
-                    </button>
-                    <button
-                        className={`bg-red-400 text-black font-medium p-[0.5rem] rounded-lg transition-colors duration-400 cursor-pointer ${status === 'absent' ? 'hover:cursor-default opacity-40' : ''}`}
-                    >
-                        <p>absent</p>
-                    </button>
-                    <button
-                        className={`bg-yellow-400 text-black font-medium p-[0.5rem] rounded-lg transition-colors duration-400 cursor-pointer ${status === 'sick' ? 'hover:cursor-default opacity-40' : ''}`}
-                    >
-                        <p>sick</p>
-                    </button>
-                </div>
+                <Button lecture={{courseCode, courseName, fro, to, status}} />
             </div>
         </div>
     )
