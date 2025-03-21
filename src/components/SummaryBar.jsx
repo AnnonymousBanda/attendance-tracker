dadd699ba6ab0a19afb759e150529d55c7a4082d
VinayKhedkar <arithmetic.chip@gmail.com>
2025-03-22 01:09:21 +0530

    fix: fix data attributes

--- src/components/SummaryBar.jsx
+++ src/components/SummaryBar.jsx
@@ -3,34 +3,34 @@ export default function SummaryBar({ summaryData }) {
         <div className="flex flex-col items-center justify-center w-full bg-white p-[1rem] rounded-xl gap-[1rem]">
             <h2 className="w-full">Summary</h2>
             <div className="flex flex-col gap-[0.5rem] w-full px-[0.5rem]">
-                {summaryData.labels.map((data, index) => (
+                {summaryData.map((data, index) => (
                     <div
                         key={index}
                         className="flex items-center w-full justify-center gap-[0.5rem]"
                     >
                         <h3
                             className={`${
-                                summaryData.data[index] > 75
+                                summaryData.presentPercentage > 75
                                     ? 'bg-green'
                                     : 'bg-red'
                             } p-[0.3rem] w-[5rem] font-charts text-center text-white font-bold rounded-lg flex-none`}
                         >
-                            {summaryData.data[index]}%
+                            {summaryData.presentPercentage}%
                         </h3>
                         <div className="flex-grow w-full h-[2.5rem] bg-gray-100 rounded-lg relative overflow-hidden">
                             <div
                                 className={`absolute top-0 left-0 h-full transition-all duration-500 ease-out ${
-                                    summaryData.data[index] < 75
+                                    summaryData.presentPercentage < 75
                                         ? 'bg-red-400'
                                         : 'bg-green-400'
                                 }`}
                                 style={{
-                                    width: `${summaryData.data[index]}%`,
+                                    width: `${summaryData.presentPercentage}%`,
                                 }}
                             />
                         </div>
                         <h3 className="lg:ml-[0.5rem] min-w-[6rem] text-right font-charts">
-                            {data}
+                            {data.courseCode}
                         </h3>
                     </div>
                 ))}
