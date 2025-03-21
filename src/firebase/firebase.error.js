c6fd6f0b0b6d211c95c1890219f6dd63b5757db3
Ankit Bhagat <annonymous007banda@gmail.com>
2025-03-21 12:20:32 +0530

    fix: fix error logs

--- src/firebase/firebase.error.js
+++ src/firebase/firebase.error.js
@@ -1,48 +1,51 @@
 class AppError extends Error {
-	constructor(message, status) {
-		super(message)
-		this.status = status
-		this.isOperational = true
-		Error.captureStackTrace(this, this.constructor)
-	}
+    constructor(message, status) {
+        super(message)
+        this.status = status
+        this.isOperational = true
+        Error.captureStackTrace(this, this.constructor)
+    }
 }
 
 const catchAsync = (fn) => {
-	return async (...params) => {
-		try {
-			return await fn(...params)
-		} catch (error) {
-			console.log('Global error handler:')
-			return globalErrorHandler(error)
-		}
-	}
+    return async (...params) => {
+        try {
+            return await fn(...params)
+        } catch (error) {
+            console.log('Global error handler:')
+            return globalErrorHandler(error)
+        }
+    }
 }
 
 const globalErrorHandler = (error) => {
-	if (process.env.NEXT_PUBLIC_ENV === 'development')
-		handleDevelopmentError(error)
-	else handleProductionError(error)
+    if (process.env.NEXT_PUBLIC_ENV === 'development') {
+        return handleDevelopmentError(error)
+    }
+    return handleProductionError(error)
 }
 
 const handleDevelopmentError = (error) => {
-	return {
-		status: error.status || 500,
-		message: error.message,
-		stack: error.stack,
-	}
+    console.error(error)
+    return {
+        status: error.status || 500,
+        message: error.message,
+        stack: error.stack,
+    }
 }
 
 const handleProductionError = (error) => {
-	if (error.isOperational) {
-		return {
-			status: error.status,
-			message: error.message,
-		}
-	}
-	return {
-		status: 500,
-		message: 'Something went wrong',
-	}
+    if (error.isOperational) {
+        return {
+            status: error.status,
+            message: error.message,
+        }
+    }
+    console.error('Unexpected error:', error)
+    return {
+        status: 500,
+        message: 'Something went wrong',
+    }
 }
 
 export { AppError, catchAsync }
