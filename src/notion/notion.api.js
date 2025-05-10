41c1dd718f1141f2c16779784767d68a77e12c27
VinayKhedkar <arithmetic.chip@gmail.com>
2025-05-10 12:37:48 +0530

    fix: fix gsheets integration

--- src/notion/notion.api.js
+++ src/notion/notion.api.js
@@ -1,50 +1,29 @@
 import { AppError, catchAsync } from '@/firebase/firebase.error'
 
 const getCourses = catchAsync(async (semester, branch) => {
-    const url = process.env.NEXT_PUBLIC_GSHEET_URL
-
-    const response = await fetch(
-        `${url}?action=getcourses&semester=${semester}`,
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                branch: branch,
-            }),
-        }
-    )
-
-    if (!response.ok) {
-        throw new AppError('Failed to fetch courses', 404)
-    }
-
-    const data = await response.json()
-    return data
+    const data = await getDatabaseLinks()
 })
 
 const getLectures = catchAsync(async (semester, day, branch) => {
+    const data = await getDatabaseLinks()
+})
+
+const getDatabaseLinks = catchAsync(async () => {
     const url = process.env.NEXT_PUBLIC_GSHEET_URL
 
-    const response = await fetch(
-        `${url}?action=getlectures&semester=${semester}&day=${day}`,
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                branch: branch,
-            }),
-        }
-    )
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
 
     if (!response.ok) {
-        throw new AppError('Failed to fetch lectures', 404)
+        throw new AppError('Failed to fetch database links', 404)
     }
 
     const data = await response.json()
+    console.log(data)
     return data
 })
 
