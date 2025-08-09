d5ca9475d0ff36514d11542c2c96eda7511555cc
VinayKhedkar <arithmetic.chip@gmail.com>
2025-08-10 00:03:29 +0530

    fix: fix gsheet data cell ranges

--- src/notion/notion.api.js
+++ src/notion/notion.api.js
@@ -4,11 +4,11 @@ const cellrange = {
     1: 'A, B, C, D, E, F, G, H, I, J, K, L, M',
     2: 'O, P, Q, R, S, T, U, V, W, X, Y, Z, AA',
     3: 'AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL, AM, AN, AO',
-    4: 'AP, AQ, AR, AS, AT, AU, AV, AW, AX, AY, AZ, BA, BB',
-    5: 'BC, BD, BE, BF, BG, BH, BI, BJ, BK, BL, BM, BN, BO',
-    6: 'BP, BQ, BR, BS, BT, BU, BV, BW, BX, BY, BZ, CA, CB',
-    7: 'CC, CD, CE, CF, CG, CH, CI, CJ, CK, CL, CM, CN, CO',
-    8: 'CP, CQ, CR, CS, CT, CU, CV, CW, CX, CY, CZ, DA, DB',
+    4: 'AQ, AR, AS, AT, AU, AV, AW, AX, AY, AZ, BA, BB, BC',
+    5: 'BE, BF, BG, BH, BI, BJ, BK, BL, BM, BN, BO, BP, BQ',
+    6: 'BS, BT, BU, BV, BW, BX, BY, BZ, CA, CB, CC, CD, CE',
+    7: 'CG, CH, CI, CJ, CK, CL, CM, CN, CO, CP, CQ, CR, CS',
+    8: 'CU, CV, CW, CX, CY, CZ, DA, DB, DC, DD, DE, DF, DG',
 }
 
 const getCourses = catchAsync(async (semester, branch) => {
